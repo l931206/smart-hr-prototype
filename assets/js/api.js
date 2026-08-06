76ff279e1c69bbd7421c23fdee09c7920b526fbf
l931206 <lydia9312062004@gmail.com>
2026-08-06 15:48:10 +0800

    feat: point frontend to deployed API

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-const HR_API_BASE_URL = window.HR_API_BASE_URL || "http://127.0.0.1:8000/api";
+const HR_API_BASE_URL = window.HR_API_BASE_URL || "https://smart-hr-api-8rxh.onrender.com/api";
 
 async function apiRequest(path, options = {}) {
   const token = localStorage.getItem("hr_token");
